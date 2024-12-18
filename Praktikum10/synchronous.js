13c9f7ff0bc6d442f163487b1d3a1932101a5122
asayahandatgr <145966599+asayahandatgr@users.noreply.github.com>
2024-12-18 15:04:47 +0700

    Delete Praktikum10 directory

--- Praktikum10/synchronous.js
+++
@@ -1,9 +0,0 @@
-console.log("Tegar Membuka Bowser Google Chrome.");
-
-console.log(`
-Tegar download file selama 1 jam.
-  Downloading 1 Hour ...
-  Download Complete.
-`);
-
-console.log("Tegar membuka Youtube.");
